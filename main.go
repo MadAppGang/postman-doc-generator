7e00e68ee1d6376a43ac87f6a335e643b63b787f
Aleksandr Shevchenko <axelsheva94@gmail.com>
2018-04-26 13:25:35 +0300

    Refactor flags

--- main.go
+++ main.go
@@ -4,50 +4,39 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 	"strings"
 )
 
 const (
 	appName               = "postman-doc-generator"
-	defaultInputFilename  = "postman_collection_tpl.json"
 	defaultOutputFilename = "postman_collection.json"
-	defaultDirectory      = "."
+	defaultDir            = "."
 )
 
 var (
-	structNames = flag.String("struct", "", "comma-separated list of struct names; must be set")
-	input       = flag.String("input", defaultInputFilename, "file name of postman collection with keywords for replacement they to models")
-	output      = flag.String("output", defaultOutputFilename, "output file name")
-	dir         = flag.String("dir", defaultDirectory, "directory to find structures")
+	flagStruct = flag.String("struct", "", "comma-separated list of struct names")
+	flagFile   = flag.String("file", "", "go filename to be parsed")
+	flagDir    = flag.String("dir", defaultDir, "directory to be parsed")
+	flagOutput = flag.String("output", defaultOutputFilename, "postman collection filename")
 )
 
 func main() {
 	log.SetFlags(0)
 	log.SetPrefix(appName + ": ")
 	flag.Parse()
-	if len(*structNames) == 0 {
-		flag.Usage()
-		os.Exit(2)
+
+	var structs []string
+	if len(*flagStruct) > 0 {
+		structs = strings.Split(*flagStruct, ",")
 	}
 
-	if !isDirectory(*dir) {
-		log.Fatalf("%s is not directory", *dir)
+	if *flagFile != "" {
+		// ParseFile *flagFile
+	} else {
+		// ParseDir *flagDir
 	}
 
-	structs := strings.Split(*structNames, ",")
-
-	fmt.Printf("Dir: %s\n", *dir)
+	fmt.Printf("Dir: %s\n", *flagDir)
 	fmt.Printf("Structs for conversion: %+v\n", structs)
-	fmt.Printf("Input file name: %s\n", *input)
-	fmt.Printf("Output file name: %s\n", *output)
-}
-
-// isDirectory returns true if the named file is a directory
-func isDirectory(name string) bool {
-	info, err := os.Stat(name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return info.IsDir()
+	fmt.Printf("Postman filename: %s\n", *flagOutput)
 }
