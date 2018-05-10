6cebd7dd6ccf70ea217866aa6fffd3a3421b78a9
Aleksandr Shevchenko <axelsheva94@gmail.com>
2018-05-10 16:52:13 +0300

    Fix issues

--- struct_parser.go
+++ struct_parser.go
@@ -4,6 +4,9 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"strings"
+
+	"github.com/madappgang/postman-doc-generator/models"
 )
 
 // ParseFile parses the file specified by filename
@@ -49,4 +52,33 @@ func parse(filename string, src interface{}) map[string]ast.StructType {
 	}
 
 	return collectStructs(node)
+}
+
+// ParseStruct converts specified struct to model and returns it
+func ParseStruct(name string, st ast.StructType) models.Model {
+	model := models.NewModel(name)
+	var fields []models.Field
+	for _, field := range st.Fields.List {
+		fieldName := field.Names[0].Name
+		fieldType := getName(field.Type)
+		fieldDesc := getDocsByField(field)
+		f := models.NewField(fieldName, fieldType, fieldDesc)
+		fields = append(fields, f)
+	}
+	model.AddField(fields...)
+	return model
+}
+
+// getName returns name from the specified expression
+func getName(expr ast.Expr) string {
+	ident, ok := expr.(*ast.Ident)
+	if !ok {
+		return ""
+	}
+	return ident.Name
+}
+
+// getDocsByField returns the text of the comment specified by field
+func getDocsByField(f *ast.Field) string {
+	return strings.TrimSuffix(f.Doc.Text(), "\n")
 }
